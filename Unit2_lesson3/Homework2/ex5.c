cb4cd869edc2321eeee11bafffad2e6f7670ef5a
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2021-05-16 04:35:00 +0200

    solved ex5

---
+++ Unit2_lesson3/Homework2/ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char x;
+    printf("Enter a character: ");
+    scanf("%c", &x);
+    if (x >= 65 && x <= 90 || x >= 97 && x <= 122)
+        printf("%c is an alphabet", x);
+    else
+        printf("%c is not an alphabet", x);
+}
