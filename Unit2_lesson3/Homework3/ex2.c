5294bc8fddc227c3136442c8807accb9efb163dd
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2021-05-27 11:51:13 +0200

    Solved HW3 ex2

---
+++ Unit2_lesson3/Homework3/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter the numbers of data: ");
+    scanf("%d", &n);
+    float *a = malloc(sizeof(int) * n), sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d. Enter number: ", i + 1);
+        scanf("%f", &a[i]);
+        sum += a[i];
+    }
+    printf("Average = %0.2f", sum/n);
+}
