6735483357f7e7414cfd777ba23e346a9b4d5160
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2021-08-07 16:55:15 +0200

    Solved HW5 structs & unions ex1

---
+++ Unit2_lesson6/Homework5/ex1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+struct SStudnet
+{
+    char name[256];
+    int roll_number;
+    float marks;
+} student;
+
+
+int main()
+{
+    printf("Enter information of students: \n");
+
+    printf("Enter name: ");
+    fgets(student.name, 256, stdin);
+    printf("Enter roll number: ");
+    scanf("%d", &student.roll_number);
+    printf("Enter marks: ");
+    scanf("%f", &student.marks);
+
+    printf("\nDisplaying Information: \n");
+    printf("name: %s", student.name);
+    printf("Roll: %d \n", student.roll_number);
+    printf("Marks: %0.2f", student.marks);
+
+}
